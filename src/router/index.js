9a9b8e28ec0cef3db9325085857ed8b73efab169
hanna4933 <hanna.mobergandersson.4933@student.uu.se>
2022-11-24 15:43:22 +0100

    skapat ny sida för createQPartView

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ScoreBoardView.vue')
-  } 
+  },
+  {
+    path: '/creatqpart/',
+    name: 'CreateQPartView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/CreateQPartView.vue')
+  }
 
 ]
 
