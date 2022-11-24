981497a0448d005c8e1dcc484f8ed235364548c7
unknown <linnea.nilsson.8042@student.uu.se>
2022-11-24 10:29:27 +0100

    message

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ResultView.vue')
-  }
+  },
+  {
+    path: '/ScoreBoard/',
+    name: 'ScoreBoardView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ScoreBoardView.vue')
+  },
+
 ]
 
 const router = createRouter({
