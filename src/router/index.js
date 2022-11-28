89f3fc56ef481c6dafe07c6f34a6aeec2211974a
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2022-11-28 14:05:25 +0100

    Fixed måsvinge

--- src/router/index.js
+++ src/router/index.js
@@ -78,7 +78,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/createQHostView.vue')
   }
-
+  {
     path: '/ScoreBoard2/',
     name: 'ScoreBoardView2',
     // route level code-splitting
