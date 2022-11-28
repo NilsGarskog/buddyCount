d42a75b52beb286e733cc5b97fe8249e2a450ada
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2022-11-28 15:30:06 +0100

    FixedAnswerQuestion

--- src/views/AnswerQuestionView.vue
+++ src/views/AnswerQuestionView.vue
@@ -1,15 +1,19 @@
 <template>
   <body>
       <h1 class="heading">Hur många gånger har du gråtit inatt? </h1>
-  <div class="characterRow">
-    <div class="characterColumn" v-for="player in avatars"
-         v-bind:avatar="avatar" v-bind:key="player.image">
-            <span class="nameAndImg">
-                {{avatar.name}}
-          <img  class="characters" :src="require('../Icons/'+avatar.image + '.png')" :key="avatar.image" v-on:click="selectThisCharacter(avatar)"/>
-            </span>
 
+  <section class="playerListContainer">
+    <div class="playerList">
+    <div v-for="player in players"
+         v-bind:players="players" v-bind:key="player.image">
+            <span class="playerNameInList">
+          <img  class="avatarImage" :src="require('../Icons/'+player.image + '.png')" :key="player.image" />
+            {{player.name}}
+            </span>
     </div>
+    </div>
+  </section>
+  <div class="answers">
   </div>
     </body>
 </template>
@@ -19,9 +23,7 @@ export default {
 name: "AnswerQuestionView",
 data: function () {
     return {
-        Players: ["Nils","Pedro","Karin","Pelle","Lotta"],
-        Answers: [5,10,54,2,0],
-        avatars: [
+        players: [
         {
           id: "Avatar_1",
           image: "Paul",
@@ -46,22 +48,49 @@ data: function () {
           id: "Avatar_5",
           image: 'ScareCrow',
           name: "Lotta"
-        }
-      ],
+        },
+              ]
+
     }
+},
 }
-}
+
 </script>
 
 <style scoped>
-.characterRow {
-    display: flex;
-    width: 100%;
-    align-content: flex-start;
-    flex-flow: column wrap;
+body{
+  position: fixed;
+  background-color: #24a07b;
+  width: 100vw;
+  min-height: 100vh;
+  padding: 0;
 }
-.nameAndImg {
-    height: 0.4em;
+.playerList {
+    text-align: left;
+    width: 100%;
+    height: 6em;
+    font-family: Righteous;
+    font-weight: 100;
+    font-size: 3em;
+    padding-top: 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: space-evenly;
+
+
+
+}
+
+.playerListContainer {
+    display: flex;
+    justify-content: center;
+    
+}
+.avatarImage {
+    width: 50px;
+    margin-bottom: -5px;
 }
 
 </style>
