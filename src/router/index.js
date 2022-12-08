000b5cee87018d087e61e039725b0e65a1cb5d60
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2022-12-08 16:06:47 +0100

    ChangedOrderOfGame

--- src/router/index.js
+++ src/router/index.js
@@ -104,12 +104,21 @@ const routes = [
 },
 
 {
-  path: '/guessQuestion/:lang/:id',
+  path: '/guessQuestion/:lang/:id/:playid',
   name: 'AnsweQuestionView',
   // route level code-splitting
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
   component: () => import(/* webpackChunkName: "about" */ '../views/GuessQuestionView.vue')
+},
+
+{
+  path: '/Waiting/:lang/:id/:playid',
+  name: 'AnsweQuestionView',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/WaitingForPlayersView.vue')
 }
 
 
