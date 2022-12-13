bfab0a312113b90f7834eb96e7e96950f84c55ae
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2022-12-13 12:07:25 +0100

    GuessQuestionViewFuncDone

--- src/views/GuessQuestionView.vue
+++ src/views/GuessQuestionView.vue
@@ -1,128 +1,141 @@
 <template>
   <body>
       <h1 class="heading">Hur många gånger har du gråtit inatt? </h1>
+      {{randomAnswers}}
+      {{GuessArray}}
+        <table>
+    <tr>
+      <th>Player</th>
+      <th>Guess</th>
+    </tr>
+    <tr v-for="player in GuessArray" :key="player.name">
+      <td>{{ player.name }}</td>
+      <td>
+        <v-select
+          :options="randomAnswers"
+          :modelValue="player.Guess"
+          @option:selected="(selectedOption) => onSelect(selectedOption)"
+          @update:modelValue="(Guess) => updateGuess(player, Guess)"
+        />
+      </td>
+    </tr>
+  </table>
+  <button v-if="randomAnswers.length===0"> Send </button>
+  
   <section class="playerListContainer">
     <div class="playerList">
-    <div v-for="(player) in players"
-         v-bind:players="players" v-bind:key="player.image">
-            <span class="playerNameInList">
-          <img  class="avatarImage" :src="require('../Icons/'+player.image + '.png')" :key="player.image" draggable="true"/>
-            {{player.name}}
-            <form id="Guesses">
-              <select width=300 
-                      style="width: 350px">
-                <option value='blue'>Blue</option>
-                <option value='green'>Green</option>
-                <option value='red'>Red</option>
-                <option value='yellow'>Yellow</option>
-                <option value='' selected>Select a Color</option>
-              </select>
-            </form>
-            </span>
-    </div>
+    
     </div>
   </section>
 
-  <!-- <div class="gridwrapper">
-    <div class="playerGrid">
-       <div v-for="(player) in players"
-         v-bind:players="players" v-bind:key="player.image">
-      <div class="avatar1"> <img  class="avatarImage" :src="require('../Icons/'+player.image + '.png')" :key="player.image" draggable="true"/></div>
-      <div class="name1">{{player.name}}</div>
-      <div class="dropBox1">dropBox</div>
-      <div class="avatar2"></div>
-      <div class="name2"></div>
-      <div class="dropBox2"></div>
-      </div>
-    </div>
-  </div>
-    {{players}}{{items}} -->
-
-  <!-- <draggable 
-  v-model="items" 
-  group="people" 
-  @start="drag=true" 
-  @end="drag=false" 
-  item-key="id">
-  <template #item="{element}">
-    <div>{{element.title}} {{element.name}}</div>
-   </template>
-</draggable>
-
-<br>
-<br>
-
- <draggable 
-  v-model="players" 
-  group="people" 
-  @start="drag=true" 
-  @end="drag=false" 
-  item-key="id">
-  <template #item="{element}">
-    <div class="playerNameInList">
-          <img  class="avatarImage" :src="require('../Icons/'+element.image + '.png')" :key="element.image" />
-            {{element.name}}
-    {{element.name}} {{element.title}}</div>
-   </template>
-</draggable> -->
 
     </body>
 </template>
 
 <script>
-// import draggable from 'vuedraggable'
+
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
+
 
 export default {
+  
 name: "GuessQuestionView",
         components: {
-            // draggable,
+            vSelect,
         },
 data: function () {
     return {
       drag: false,
         players: [
-        {
-          id: "Avatar_1",
-          image: "Paul",
-          name: "Nils"
-        },
-        {
-          id: "Avatar_2",
-          image: 'Jerome',
-          name: "Pedro"
-        },
-        {
-          id: "Avatar_3",
-          image: 'NoFace',
-          name: "Karin"
-        },
-        {
-          id:"Avatar_4",
-          image: 'Mononoke',
-          name: "Pelle"
-        },
-        {
-          id: "Avatar_5",
-          image: 'ScareCrow',
-          name: "Lotta"
-        },
+        { "playerId": 1, "name": "Nils", "avatar": [ { "id": "Avatar_1", "image": "Paul" } ], "points": 0, "currentAnswer": 0, "currentGuess": [] }
+        ,
+        { "playerId": 123456, "name": "Samuel", "avatar": [ { "id": "Avatar_2", "image": "Jerome" } ], "points": 0, "currentAnswer": 0, "currentGuess": [] }
+        ,
+        { "playerId": 654321, "name": "Karin", "avatar": [ { "id": "Avatar_3", "image": "NoFace" } ], "points": 0, "currentAnswer": 0, "currentGuess": [] }
+        ,
+        { "playerId": 135791, "name": "Pelle", "avatar": [ { "id": "Avatar_4", "image": "Mononoke" } ], "points": 0, "currentAnswer": 0, "currentGuess": [] }
+        ,
+        { "playerId": 123654, "name": "Lotta", "avatar": [ { "id": "Avatar_5", "image": "ScareCrow" } ], "points": 0, "currentAnswer": 0, "currentGuess": [] }
               ],
-      items:[
-      { id: 1, title: '24'},
-      { id: 2, title: '54'},
-      { id: 3, title: '11'},
-      { id: 4, title: '2'},
-      { id: 5, title: '3'},
+      answers:[
+      {PlayerId: 1, Answer:20},
+      {PlayerId: 123456, Answer:4},
+      {PlayerId: 654321, Answer:2},
+      {PlayerId: 135791, Answer:15},
+      {PlayerId: 123654, Answer:1},
 
-    ]
+    ],
+    GuessArray:[],
+    randomAnswers:[],
 
     }
 },
+created: function() {
+  this.randomAnswers = randomAns(this.answers, this.randomAnswers)
+  this.GuessArray = createGuessArr(this.players)
+
+
+},
+events: { 'option:selected': function (selectedOption) {console.log("TESTIS!" + selectedOption) } },
+methods: {
+    updateGuess: function(player,Guess) {
+      console.log(Guess)
+      if (player.Guess != "" && player.Guess != null){
+        this.randomAnswers.push(player.Guess)
+      }
+      else if (Guess === null){
+        this.randomAnswers.push(player.Guess)
+      }
+      player.Guess = Guess
+    },
+onSelect: function(selectedOption) {
+
+      let index = null;
+      for (var i = 0, l = this.randomAnswers.length; i < l; i++){
+        if (selectedOption === this.randomAnswers[i]){
+           index = i
+        }
+      }
+      if (index != null){
+      this.randomAnswers.splice(index,1)
+      
+      }
+},
+},
+
+
+
 }
+
+
+function randomAns (ans, randAns){
+  for (var i = 0, l = ans.length; i < l; i++){
+    randAns.push(ans[i].Answer)
+  }
+  randAns.sort(() => Math.random() - 0.5);
+  return randAns
+}
+
+function createGuessArr (players){
+  var GuessArray = []
+  for (var i = 0, l = players.length; i < l; i++){
+    var obj = {playerId: players[i].playerId, name: players[i].name, avatarImg: players[i].avatar[0].image, Guess:""}
+    GuessArray.push(obj)
+}
+  return GuessArray
+}
+
+
 
 </script>
 
 <style scoped>
+table {
+  display: table;
+  width: 100%;
+}
+
 body{
   position: fixed;
   background-color: #24a07b;
@@ -130,70 +143,6 @@ body{
   min-height: 100vh;
   padding: 0;
   cursor: default;
-}
-
-.gridwrapper {
-  height:50%;
-}
-
-.playerGrid{
-  display: grid;
-
-  grid-template-columns: 0.3fr 1fr 0.3fr 0.3fr 1fr 0.3fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-
-    gap: 0px;
-   height: 50%;
-
-}
-
-.avatar1 {
-  grid-column-start: 1;
-  grid-column-end: 2;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
-
-}
-
-.name1 {
-    grid-column-start: 2;
-  grid-column-end: 3;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
-}
-
-.dropBox1 {
-      grid-column-start: 3;
-  grid-column-end: 4;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
-}
-
-.avatar2 {
-        grid-column-start: 4;
-  grid-column-end: 5;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
-}
-
-.name2 {
-          grid-column-start: 5;
-  grid-column-end: 6;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
-}
-
-.dropBox1 {
-            grid-column-start: 6;
-  grid-column-end: 7;
-
-  grid-row-start: 1;
-  grid-row-end: 6;
 }
 
 
@@ -222,13 +171,7 @@ body{
     height: 1em;
 
 }
-.dropZone{
-  background-color: grey;
-  width: 1em;
 
-
-  
-}
 
 .playerNameInList {
   display: flex;
