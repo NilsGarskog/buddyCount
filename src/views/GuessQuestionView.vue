52e39f1ea47c344c7a8ed8df80fca5bb09025790
Samuel Ram <samuel.gh.ram@gmail.com>
2022-12-14 00:44:10 +0100

    Commit

--- src/views/GuessQuestionView.vue
+++ src/views/GuessQuestionView.vue
@@ -9,26 +9,96 @@
           <img  class="avatarImage" :src="require('../Icons/'+player.image + '.png')" :key="player.image"/>
             {{player.name}}
             <div class="dropZone">
-              
+            
                </div>
             </span>
     </div>
     </div>
-  </section>
 
+    <div class="drop2zone">
+      <div v-for="item in items"
+      v-bind:items="items" v-bind:key="item.title">
+      <component :is="interact" />
+    <span class="draggable"> {{item.title}}</span>
+    </div>
+    </div>
+  </section>
+ 
 
  
     </body>
 </template>
 
 <script>
-import vSelect from 'vue-select'
+/*import vSelect from 'vue-select'*/
+import '@interactjs/auto-start'
+import '@interactjs/actions/drag'
+import '@interactjs/actions/resize'
+import '@interactjs/modifiers'
+import '@interactjs/dev-tools'
+import interact from '@interactjs/interact'
+
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    /* modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ], */
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
+
+
 
 export default {
 name: "AnswerQuestionView",
         components: {
-            vSelect,
+      interact,
         },
+
+created(){
+  
+},
+
 
 data: function () {
     return {
@@ -181,6 +251,12 @@ body{
 
 
   
+}
+
+.draggable{
+  height:2em;
+  width:2em;
+  background-color:gray;
 }
 
 .playerNameInList {
